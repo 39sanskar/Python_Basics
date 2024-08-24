3b21f8a63994a67fcd14a3c6d84620fc02caa745
39sanskar <mishrasanskar496@gmail.com>
2024-08-24 12:47:56 +0530

    list

---
+++ 01_basics/my_list.py
@@ -0,0 +1,14 @@
+##List is Mutable.
+myListOne = [1,2,3]
+myListTwo = myListOne
+##print(myListOne)
+
+myListOne = "chai"
+print(myListTwo)
+##in the above code myListOne is assign the reference velue in the memory that is [1, 2, 3].
+## in myListTwo it assign myListOne that has the acctual value in the memory which is [1, 2, 3].
+# print(myListTwo) that will be print [1, 2, 3].
+# update myListOne = "chai". that myListOne refer the value which is "chai".
+# Hence the conclusion  in myListTwo that refer the value of mylistOne which is [1,2,3]. not 
+#not the value "chai" bec of that it assign the value not the variable.
+
