22efccb42a6466e91b83e623117c5c65f7a1a44f
39sanskar <mishrasanskar496@gmail.com>
2024-08-24 13:09:22 +0530

    list

--- 01_basics/list_hack.py
+++ 01_basics/list_hack.py
@@ -1,7 +1,20 @@
 h1 = [1 , 2, 3]
-h2 = h1[:]
-# it take suru se start karke end tak haanahai 
+h2 = h1[:]##here we adding slicing then it make a copy of h1.
+# it take suru se start karke end tak jaanahai 
 print(h1)
 print(h2)
+h1[0] = 55
+print(h1)
+print(h2)
+
+# that is also use in python 
+# impot copy
+# h2 = copy.copy(h1)
+
+# that is also use in python 
+# impot copy
+# h2 = copy.deepcopy(h1) ## that is uses in the copy for lint in list.
+
+
 
 
